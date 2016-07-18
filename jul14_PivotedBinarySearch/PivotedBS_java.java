b95acab85128e83936010fe00941f0fb16045f26
adykumar <adykumar01@gmail.com>
2016-07-18 00:53:37 -0700

    pivoted BS

--- jul14_PivotedBinarySearch/PivotedBS_java.java
+++ jul14_PivotedBinarySearch/PivotedBS_java.java
@@ -6,18 +6,27 @@ class PivotedBS {
 		if (len==1) return 0;
 		int left=0;
 		int right= len-1;
-		System.out.print(left+" "+right+" ");
-		while(arr.get(mid)<arr.get(mid+1) && left) {
+		//System.out.print(left+" "+right+" ");
+		while(right-left>1) {
 			int mid= (left+right)/2;
-			if(arr.get(left)<arr.get(mid))  left=mid+1;
-			if(arr.get(right)>arr.get(mid)) right=mid-1;
+			if(arr.get(left)<=arr.get(mid))  left=mid;
+			if(arr.get(right)>=arr.get(mid)) right=mid;
 		}
-		return left;
+		return right;
 	}
 
-	public static int findPivotedBinary(List<Integer> arr, int len) {
-		int pivot= findPivot(arr, len);
-		return pivot;
+	public static int findPivotedBinary(List<Integer> arr, int size, int X) {
+		int pivot= findPivot(arr, size);
+		System.out.print(pivot+" ");
+		int left=0;
+		int right= size-1;
+		while(left<=right) {
+			int mid= (left+right)/2;
+			if(arr.get( ((mid+pivot) % size)) == X ) return (mid+pivot)%size;
+			else if (arr.get( ((mid+pivot) % size)) > X ) right= mid-1;
+			else left= mid+1; 
+		}
+		return -1;
 	}
 
 	public static void main(String args[]) {
@@ -31,7 +40,7 @@ class PivotedBS {
 			}
 			int X= scanner.nextInt();
 
-			System.out.println(findPivotedBinary(arr, arr.size()));
+			System.out.println(findPivotedBinary(arr, arr.size(), X));
 		}
 	}
 }
