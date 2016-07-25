db284dbe266b7bda888373641670b055a9bd1001
adykumar <adykumar01@gmail.com>
2016-07-24 20:53:02 -0700

    working median of sorted arrays

--- jul15_MedianSortedArrays/MedianSortedArrays_java.java
+++ jul15_MedianSortedArrays/MedianSortedArrays_java.java
@@ -3,22 +3,43 @@ import java.util.*;
 class MedianSortedArrays {
 
 	public static double findMedian(List<Integer> arr1, List<Integer> arr2, int size) {
+		/*
+		for(int a=0;a<size;a++) System.out.print(arr1.get(a)+" ");
+		System.out.println();
+		for(int a=0;a<size;a++) System.out.print(arr2.get(a)+" ");
+		System.out.println();*/
 		if (size ==1) return (arr1.get(0)+arr2.get(0))/2;
-		//if (size%2==1 && arr1.get(size/2)==arr2.get(size/2)) return arr1.get(size/2);
-		//if (size%2==0 && arr1.get(size/2)+arr1.get((size-1)/2) == arr1.get(size/2)+arr1.get((size-1)/2) ) {
-		//	return 0.5*((arr1.get(size/2)+arr1.get((size-1)/2)));
-		int mid= size/2;
+		if (size ==2) {
+			float sum= arr1.get(0)+arr1.get(1)+arr2.get(0)+arr2.get(1);
+			int min= arr1.get(0) < arr2.get(0) ? arr1.get(0) : arr2.get(0);
+			int max= arr1.get(1) > arr2.get(1) ? arr1.get(1) : arr2.get(1);
+			return (sum-min-max)/2;			
+		}
+		int mid= (size-1)/2;
+		int median1; int median2;
+		if (size%2==1) {
+			median1= arr1.get(mid);
+			median2= arr2.get(mid);
+		}
+		else {
+			median1= arr1.get(mid)+arr1.get(mid+1);
+			median2= arr2.get(mid)+arr2.get(mid+1);
+		}
+
 		if (arr1.get(mid)<arr2.get(mid)) {
-			for(int i=0;i<mid;i++) arr1.remove(i);
-			//for(int i=(mid+1)/2;i<size-1;i++) arr2.remove(i);
+			for(int i=0,j=size-1; i<mid; i++,j--) {
+				arr1.remove(0);
+				arr2.remove(j);
+			}
 		}
 		else  {
-			for(int i=0;i<mid;i++) arr2.remove(i);
-			//for(int i=(mid+1)/2;i<size-1;i++) arr1.remove(i);
+			//System.out.println("--");
+			for(int i=0,j=size-1; i<mid; i++,j--) {
+				arr1.remove(j);
+				arr2.remove(0);
+			}
 		}
-		System.out.println(arr1.size()+" "+arr2.size());
-		//findMedian(arr1,arr2,arr1.size());
-		return 0;
+		return findMedian(arr1,arr2,arr1.size());
 	}
 
 	public static void main(String args[]) {
