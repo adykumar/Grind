5c8ee7bdbf0417e2f4fb9fb5e94081c99c24d288
adykumar <adykumar01@gmail.com>
2016-07-25 21:57:00 -0700

    stack using array fails

---
+++ jul21_StackUsingArray/StackUsingArray_java.java
@@ -0,0 +1,33 @@
+import java.util.*;
+/* 	supports CreateStack, Pop, Peek, Push, isFull, isEmpty operations
+		Stack Using Array:
+		C - Capacity of Stack
+		N - Number of elements to push on stack followed by N elements
+		P - Number of elements to Pop
+		O/P - Popped elements. Finally followed by O/P of Peek.
+*/
+
+class StackUsingArray {
+	public static void main(String args[]) {
+		Scanner scanner= new Scanner(System.in);
+		int C= scanner.nextInt();
+		int N= scanner.nextInt();
+		if (N>C) {System.out.println("Over capacity!"); return;}
+		Stackops myStack;
+		myStack.createStack(C);
+		for(int i=0;i<N;i++) {
+			myStack.push(scanner.nextInt());
+		}
+		int P= scanner.nextInt();
+		for(int j=0;j<P;j++) {
+			if (! (myStack.isEmpty())) {
+				int ele=myStack.pop();
+				System.out.println("Popped:"+ele);
+			}
+			else System.out.println("Cant pop, empty");
+		}
+
+		int x= myStack.peek();
+		System.out.println("Peeked:" +x);
+	}
+}
---
+++ jul21_StackUsingArray/Stackops.java
@@ -0,0 +1,42 @@
+public class Stackops {
+
+	int[] stack;
+	int top;
+	int Cap;
+
+	public void createStack(int C) {
+		Cap=C;
+		stack = new int[Cap];
+		top=-1;
+
+	}
+
+	public void push(int x) {
+			if (top>Cap) return;
+			top++;
+			stack[top]=x;
+	}
+
+	public int pop() {
+		if (top != -1) {
+			top--;
+			return stack[top+1];
+		}
+		return 0; 
+	}
+
+	public boolean isEmpty() {
+		if (top==-1) return true;
+		return false;
+	}
+
+	public boolean isFull() {
+		if (top>Cap) return true;
+		return false;
+	}
+
+	public int peek() {
+		return stack[top];
+	}
+
+}
