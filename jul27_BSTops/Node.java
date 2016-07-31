e996a1ff7eca0afd8ed109420b190cf8a0c6f436
adykumar <adykumar01@gmail.com>
2016-07-31 11:40:55 -0700

    working on BSTops

---
+++ jul27_BSTops/Node.java
@@ -0,0 +1,25 @@
+class bstNode {
+
+	int data;
+	Node left;
+	Node right;
+}
+
+class BST {
+
+	Node head;
+
+	public BST() {
+		head= new bstNode();
+		head.data=0;
+		head.left=null;
+		head.right=null;
+	}
+
+	public insert(int x) {
+		trav=head;
+		while(trav
+
+			)
+	}
+}
